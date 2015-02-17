0a0ab0d3814d38d6dea19fb7a5c39173f40465c5
Mateusz Nowak <mateusz.nowak@pmrcorporate.com>
2015-02-17 17:24:45 +0100

    zad 1

---
+++ db.php
@@ -0,0 +1,27 @@
+<?php
+	ini_set('display_errors',1);
+	ini_set('display_startup_errors',1);
+	error_reporting(-1);
+
+	class Database extends mysqli{
+
+
+		private $dbname = 'tasks';
+		private $dbuser = 'root';
+		private $dbpass = '';
+		private $dbhost = '';
+
+		public function connect() {
+			$mysqli = new mysqli($this->dbhost, $this->dbuser, $this->dbpass, $this->dbname);
+			if ($mysqli->connect_errno) {
+	    		echo "Błąd połączenia: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+			}
+			else {
+				return $mysqli;
+			}
+		}
+
+	}
+	
+
+?>
--- zadanie1.php
+++ zadanie1.php
@@ -2,29 +2,95 @@
 	include('db.php');
 
 	ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(-1);
+	ini_set('display_startup_errors',1);
+	error_reporting(-1);
 
 	class Zadanie1 {
+		private $conn;
 
-		//private $dbconn;
-
-		public function __construct() {
+		public function __construct($count) {
 			$dbconn = new Database();
-			$this->createDbTable($dbconn);
+			$this->conn = $dbconn->connect();
+			if($this->conn) {
+				$this->createDbTable($this->conn);
+				//$this->generateNodes($count);
+				$this->showTree();
+			}
 		}
-
 
 		private function createDbTable($dbconn) {
-
-			$dbconn->query("CREATE TABLE zad1(id int);");
-			
+			$dbconn->query("CREATE TABLE zad1(id int NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL, parent_id int, PRIMARY KEY (id));");
 		}
 
+		private function generateNodes($counter) {
+			for($i=0; $i<$counter; $i++) {
+				$random = rand(0, 1);
+				if($random == 0) {
+					$parentId = $this->getParent();
+					if($parentId === null) {
+						$this->addParent();
+					}
+					else {
+						$this->addChild($parentId);
+					}
+				}
+				else {
+					$this->addParent();
+				}
+			}
+		}
 
+		private function getParent() {
+			$result = $this->conn->query("SELECT id FROM zad1 WHERE parent_id IS NULL");
+			$lenght = $result->num_rows;
+			$ids = array();
+			if($lenght > 0) {
+				while ($obj = $result->fetch_object()) {
+					$ids[] = $obj->id;
+				}
+				$id = rand(0,$lenght-1);
+				return $ids[$id];
+			}
+			else {
+				return null;
+			}
+		}
+
+		public function getChild($id) {
+			$child = array();
+			$result = $this->conn->query("SELECT * FROM zad1 WHERE parent_id = $id;");
+			while($row = $result->fetch_assoc()) {
+				$child[] = array('name' =>$row['name'], 'children' => '');
+			};
+			return $child;
+		}
+
+		private function addParent() {
+			$result = $this->conn->query("INSERT INTO zad1 VALUES('','Parent', NULL);");
+		}
+
+		private function addChild($parent) {
+			$this->conn->query("INSERT INTO zad1 VALUES('', 'Child', $parent);");
+		}
+
+		private function showTree() {
+			$tree = array();
+			$result = $this->conn->query("SELECT * FROM zad1 WHERE parent_id IS NULL;");
+			
+			while($row = $result->fetch_assoc()) {
+				$child = $this->getChild($row['id']);
+				$tree[] = array('name' =>$row['name'], 'children' => $child);
+			}
+			
+			echo "<pre>";		
+				var_export($tree);
+			echo "</pre>";
+
+		}
 	}
 
-	$a = new Zadanie1();
+	$a = new Zadanie1(10);
+	$a->getChild(73);
 
 
 
