dd7cdb7ddf53a05a0aca237c6b0f2ab9f76b49fa
Mateusz Nowak <mateusz.nowak@pmrcorporate.com>
2015-02-20 18:10:48 +0100

    encoding db

--- db.php
+++ db.php
@@ -17,6 +17,7 @@
 	    		echo "Błąd połączenia: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 			}
 			else {
+				$mysqli->query("SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'");
 				return $mysqli;
 			}
 		}
